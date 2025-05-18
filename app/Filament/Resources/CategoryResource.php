5fe8b6626b545802d4a5e265d68504cf6814b124
irahat120 <rahat@elitbuzz-bd.com>
2025-05-18 14:38:02 +0600

    admin role

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -25,6 +25,7 @@ class CategoryResource extends Resource
     protected static ?string $model = Category::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-tag';
+    protected static ?string $navigationGroup = 'Product Management';
 
     public static function form(Form $form): Form
     {
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,15 +3,25 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Panel;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->hasRole('Admin');
+    }
     /**
      * The attributes that are mass assignable.
      *
@@ -19,10 +29,10 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'user_name',
         'email',
         'password',
     ];
+
 
     /**
      * The attributes that should be hidden for serialization.
